17e439bdf2133d308fc0c9bd0b0cfd989f3480b7
Leonardo Razovic <leorazzo@gmail.com>
2021-06-02 11:05:18 +0200

    feat(Incrementer): value increment by an i32

--- incrementer/lib.rs
+++ incrementer/lib.rs
@@ -4,33 +4,31 @@ use ink_lang as ink;
 
 #[ink::contract]
 mod incrementer {
+
     #[ink(storage)]
     pub struct Incrementer {
-        flag: bool,
         value: i32,
     }
 
     impl Incrementer {
         #[ink(constructor)]
         pub fn new(init_value: i32) -> Self {
-            // Contract Constructor
-            Self {
-                flag: false,
-                value: init_value,
-            }
+            Self { value: init_value }
         }
 
         #[ink(constructor)]
         pub fn default() -> Self {
-            Self {
-                flag: false,
-                value: 0,
-            }
+            Self { value: 0 }
         }
 
         #[ink(message)]
         pub fn get(&self) -> i32 {
             self.value
+        }
+
+        #[ink(message)]
+        pub fn inc(&mut self, by: i32) {
+            self.value += by
         }
     }
 
@@ -43,6 +41,16 @@ mod incrementer {
         fn default_works() {
             let contract = Incrementer::default();
             assert_eq!(contract.get(), 0);
+        }
+
+        #[ink::test]
+        fn it_works() {
+            let mut contract = Incrementer::new(42);
+            assert_eq!(contract.get(), 42);
+            contract.inc(5);
+            assert_eq!(contract.get(), 47);
+            contract.inc(-50);
+            assert_eq!(contract.get(), -3);
         }
     }
 }
