bb838905c6ae90b2218902079fc8acab41b5f7ca
Leonardo Razovic <leorazzo@gmail.com>
2021-06-02 12:34:16 +0200

    feat(ERC20): transfer tokens from address to addrress

--- erc20/lib.rs
+++ erc20/lib.rs
@@ -16,13 +16,11 @@ mod erc20 {
     impl Erc20 {
         #[ink(constructor)]
         pub fn new(initial_supply: Balance) -> Self {
-            // ACTION: `set` the total supply to `initial_supply`
-            // ACTION: `insert` the `initial_supply` as the `caller` balance
             let mut balances = ink_storage::collections::HashMap::new();
             balances.insert(Self::env().caller(), initial_supply);
             Self {
                 total_supply: initial_supply,
-                balances: balances
+                balances,
             }
         }
 
@@ -33,15 +31,40 @@ mod erc20 {
 
         #[ink(message)]
         pub fn balance_of(&self, owner: AccountId) -> Balance {
-            // ACTION: Return the balance of `owner`
-            //   HINT: Use `balance_of_or_zero` to get the `owner` balance
             self.balance_of_or_zero(&owner)
         }
 
+        #[ink(message)]
+        pub fn transfer(&mut self, to: AccountId, value: Balance) -> bool {
+            // ACTION: Call the `transfer_from_to` with `from` as `self.env().caller()`
+            self.transfer_from_to(self.env().caller(), to, value)
+        }
+
+        fn transfer_from_to(&mut self, from: AccountId, to: AccountId, value: Balance) -> bool {
+            // ACTION: Get the balance for `from` and `to`
+            //   HINT: Use the `balance_of_or_zero` function to do this
+            // ACTION: If `from_balance` is less than `value`, return `false`
+            // ACTION: Insert new values for `from` and `to`
+            //         * from_balance - value
+            //         * to_balance + value
+            // ACTION: Return `true`
+            let from_balance = self.balance_of_or_zero(&from);
+            if from_balance < value {
+                false
+            } else {
+                self.balances
+                    .entry(from)
+                    .and_modify(|old_value| *old_value -= value);
+                self.balances
+                    .entry(to)
+                    .and_modify(|old_value| *old_value += value)
+                    .or_insert(value);
+                true
+            }
+        }
+
         fn balance_of_or_zero(&self, owner: &AccountId) -> Balance {
-            // ACTION: `get` the balance of `owner`, then `unwrap_or` fallback to 0
-            // ACTION: Return the balance
-            *self.balances.get(owner).unwrap_or(&0u128)
+            *self.balances.get(owner).unwrap_or(&0)
         }
     }
 
@@ -63,6 +86,15 @@ mod erc20 {
             assert_eq!(contract.total_supply(), 100);
             assert_eq!(contract.balance_of(AccountId::from([0x1; 32])), 100);
             assert_eq!(contract.balance_of(AccountId::from([0x0; 32])), 0);
+        }
+
+        #[ink::test]
+        fn transfer_works() {
+            let mut contract = Erc20::new(100);
+            assert_eq!(contract.balance_of(AccountId::from([0x1; 32])), 100);
+            assert!(contract.transfer(AccountId::from([0x0; 32]), 10));
+            assert_eq!(contract.balance_of(AccountId::from([0x0; 32])), 10);
+            assert!(!contract.transfer(AccountId::from([0x0; 32]), 100));
         }
     }
 }
